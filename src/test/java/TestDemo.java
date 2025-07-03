f5c724bbe12cacdb5b0c96658c622e15a1b43b6b
sruthi <sruthi.kc@ubq.in>
2025-07-03 10:34:05 +0530

    commiting main class deletion

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -19,54 +19,62 @@ public class TestDemo {
 	public void GetUser() {
 		given()
 		.urlEncodingEnabled(false)
-	    .header("x-api-key","reqres-free-v1")
+		.header("x-api-key","reqres-free-v1")
 		.when()
 		.get("https://reqres.in/api/users/2")
 		.then()
 		.statusCode(200)
 		.body("data.id", equalTo(2))
 		.log().all();
-		
+
 	}
 	@Test(priority = 1)
 	public void createUser()
 	{
-		
-		Map<String, Object> data = new HashMap<>();
-	    data.put("name", "nayana");
-	    data.put("job", "HR");
 
-	  id= given()
-	      .urlEncodingEnabled(false)
-	      .header("x-api-key","reqres-free-v1")
-	      .contentType(ContentType.JSON)
-	      .body(data)
-	      .when()
-	      .post("https://reqres.in/api/users")
-	      .jsonPath().getInt("id");
-	     /* .then()
+		Map<String, Object> data = new HashMap<>();
+		data.put("name", "Maya");
+		data.put("job", "HR");
+
+		id= given()
+				.urlEncodingEnabled(false)
+				.header("x-api-key","reqres-free-v1")
+				.contentType(ContentType.JSON)
+				.body(data)
+				.when()
+				.post("https://reqres.in/api/users")
+				.jsonPath().getInt("id");
+		/* .then()
 	      .log().all()
 	      .statusCode(201);*/
 	}
 	@Test(priority = 2,dependsOnMethods = {"createUser"})
 	public void updateUser()
 	{
-		
+
 		Map<String, Object> data = new HashMap<>();
-	    data.put("name", "Saniya");
-	    data.put("job", "HR");
-	    given()
-	    .urlEncodingEnabled(false)
-	      .header("x-api-key","reqres-free-v1")
-	      .contentType(ContentType.JSON)
-	      .body(data)
-	      .when()
-	      .put("https://reqres.in/api/users"+id)
-	      .jsonPath().getInt("id");
+		data.put("name", "Tanaya");
+		data.put("job", "HR");
+		given()
+		.urlEncodingEnabled(false)
+		.header("x-api-key","reqres-free-v1")
+		.contentType(ContentType.JSON)
+		.body(data)
+		.when()
+		.put("https://reqres.in/api/users"+id)
+		.then()
+		.statusCode(201)
+		.log().all();
 	}
-	//@Test
+	@Test(priority = 2,dependsOnMethods = {"createUser"})
 	public void deleteUser()
 	{
-		
+		given()
+		.then()
+		.when()
+		.delete("https://reqres.in/api/users"+id)
+		.then()
+		.statusCode(204)
+		.log().all();
 	}
 }
